c970ff1b4bb0fed38f80f164e4998b13eb7d714b
leol <ducleoliao@gmail.com>
2017-07-31 07:58:56 -0700

    Installed webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: "./lib/entry.js",
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: "bundle.js"
+  },
+  devtool: 'source-map',
+};
